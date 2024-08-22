ceba961e6e56a2fb94214d1a15d3dac78a6d0e84
Eric <ericandradestrohmbvt2023@gmail.com>
2024-08-21 19:18:13 -0700

    Added loop to generate grid for game boards

--- app.js
+++ app.js
@@ -26,24 +26,38 @@ function flip () {
 }
 
 /**
- * Creates a div with classname 'game-board'.
+ * Creates a div with classname 'game-board' with specified player and background color.
  * Div is then attached to gamesBoardContainer
+ * Also generates 100 divs for grid placement.
+ * 
  * Style defined in styles.css.
- * Parameters: color - Color used for background color.
+ * 
+ * Parameters: 
+ * color - Color used for background color.
+ * user - Specifies if it's a player or computer.
+ * 
  * Invoked immediately.
  */
 // Board Creation
 const width = 10
-function createBoard(color) {
+function createBoard(color, user) {
     const gameBoardContainer = document.createElement('div');
     gameBoardContainer.classList.add('game-board');
     gameBoardContainer.style.backgroundColor = color;
+    gameBoardContainer.id = user;
+
+    for (let i = 0; i < width * width; i++){
+        const block = document.createElement('div');
+        block.classList.add('block');
+        block.id = i;
+        gameBoardContainer.append(block);
+    }
 
     gamesBoardContainer.append(gameBoardContainer);
 }
 
-createBoard('yellow');
-createBoard('pink');
+createBoard('yellow', 'player');
+createBoard('pink', 'computer');
 
 // ================================
 // Event Listeners
