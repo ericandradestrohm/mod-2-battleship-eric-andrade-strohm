fc85e89c5e5d728d5998bfb9a0d78d73e050f7a4
Eric <ericandradestrohmbvt2023@gmail.com>
2024-08-21 18:53:27 -0700

    Added headings for sections in JS and updated descriptions

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
+// ================================
+// DOM Queries
+// ================================
 const flipButton = document.getElementById('flip-button');
 const optionsContainer = document.querySelector('.options-container');
 
+// ================================
+// Variables
+// ================================
+// Used for flip()'s angle
 let angle = 0;
-function flip () {
-    /**
+
+// ================================
+// Functions
+// ================================
+/**
      * Toggles the rotation of all child elements within the optionsContainer
      * between 0 and 90 deg.
      * Used by: `flipButton`
      */
-
+function flip () {
     const optionsShips = Array.from(optionsContainer.children);
 
     angle = angle === 0 ? 90 : 0;
@@ -17,4 +27,7 @@ function flip () {
     }
 }
 
+// ================================
+// Event Listeners
+// ================================
 flipButton.addEventListener('click', flip);
