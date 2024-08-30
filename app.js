32c6d2297fefc9dad39dd5bc1b21023ae6fa2f05
Eric <ericandradestrohmbvt2023@gmail.com>
2024-08-29 19:54:28 -0700

    Stared code for reading user input. Adjusted addShipPiece a little to accomodate being used for user placement

--- app.js
+++ app.js
@@ -4,6 +4,7 @@
 const gamesBoardContainer = document.getElementById('gamesboard-container');
 const optionsContainer = document.querySelector('.options-container');
 const flipButton = document.getElementById('flip-button');
+const allPlayerBlocks = document.querySelectorAll('#player div');
 
 // ================================
 // Functions
@@ -55,7 +56,8 @@ function createBoard(color, user) {
 }
 
 /**
- * Generates random placements for ship pieces for Computer player.
+ * Places the pieces for the player,
+ * For the computer, generates random piece placements
  * Generates a 50-50 on if the ship will be horizontal.
  * The starting index is randomized as a block between 0 and 100.
  * Starting index is adjusted so it doesn't go off the board vertically or horizontally.
@@ -63,15 +65,17 @@ function createBoard(color, user) {
  * Runs again if placement is invalid or overlaps.
  * 
  * Parameter:
+ * user - player or CPU
  * ship - The ship Object
+ * startId - ID of the piece
  * 
  * Invoked when creating ship placements.
  */
-function addShipPiece(ship) {
-    const allBoardBlocks = document.querySelectorAll("#computer div");
+function addShipPiece(user, ship, startId) {
+    const allBoardBlocks = document.querySelectorAll(`#${user} div`);
     // 50-50 on true or false
     let randomBoolean = Math.random() < 0.5;
-    let isHorizontal = randomBoolean;
+    let isHorizontal = user === 'player' ? (angle === 0 || angle === 180): randomBoolean;
     // Generates random starting position
     let randomStartIndex = Math.floor(Math.random() * width * width);
 
@@ -102,7 +106,7 @@ function addShipPiece(ship) {
     }
     
     // CHECK TO MAKE SURE PIECES DON'T OVERFLOW TO NEW ROW
-    let valid = true;
+    let noOverflow = true;
     if (isHorizontal) {
         /*
         if horizontal, check that each piece doesn't equal a spot that will overflow
@@ -110,22 +114,38 @@ function addShipPiece(ship) {
         shipBlocks.length - (index + 1) will help iterate through each piece; index helps it count
         width - (shipBlocks.length - (index + 1)) will go through each column that the starting position can't be
         */ 
-        valid = shipBlocks.every((_shipBlock, index) => shipBlocks[0].id % width !== width - (shipBlocks.length - (index + 1)))
+        noOverflow = shipBlocks.every((_shipBlock, index) => shipBlocks[0].id % width !== width - (shipBlocks.length - (index + 1)))
     }
     // CHECK TO MAKE SURE PIECES DON'T OVERLAP WITH ANOTHER PIECE
     const notTaken = shipBlocks.every(shipBlock => !shipBlock.classList.contains('taken'));
 
     // RUNS IF VALID, OTHERWISE LOOPS
-    if(valid && notTaken){
+    if(noOverflow && notTaken){
         shipBlocks.forEach(shipBlock => {
             shipBlock.classList.add(ship.name);
             shipBlock.classList.add('taken');
         })
     } else {
-        addShipPiece(ship);
+        addShipPiece(user, ship, startId);
     }
 }
 
+// Player functions
+let draggedShip;
+const optionShips = Array.from(optionsContainer.children);
+
+function dragStart(e) {
+    draggedShip = e.target;
+}
+function dragOver(e) {
+    e.preventDefault();
+
+}
+function dropShip(e) {
+    const startId = e.target.id;
+    const ship = ships[draggedShip.id];
+    addShipPiece('player', ship);
+}
 // ================================
 // Constructors
 // ================================
@@ -154,9 +174,16 @@ const ships = [destroyer, submarine, cruiser, battleship, carrier];
 createBoard('#7fbcc7', 'player');
 createBoard('#98d0e0', 'computer');
 
-ships.forEach(ship => addShipPiece(ship))
+ships.forEach(ship => addShipPiece('computer', ship));
 
 // ================================
 // Event Listeners
 // ================================
 flipButton.addEventListener('click', flip);
+
+// For dragging player ships
+optionShips.forEach(optionShip => optionShip.addEventListener('dragstart', dragStart));
+allPlayerBlocks.forEach(playerBlock => {
+    playerBlock.addEventListener('dragover', dragOver)
+    playerBlock.addEventListener('drop', dropShip)
+});
